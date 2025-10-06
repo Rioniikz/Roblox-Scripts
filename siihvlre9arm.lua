1f67207ddeef5ad895be10d9b93b73446dabf192
Rioniikz <rionikz597@gmail.com>
2025-10-06 19:03:24 +0200

    Update script

---
+++ siihvlre9arm.lua
@@ -0,0 +1,4 @@
+local universeId = game.GameId
+if universeId == 7685678300 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Rioniikz/Roblox-Scripts/main/781jqjpn9e.txt"))()
+end
