40a0b558542697d3cf85e76e6bfe153963aecc66
Rioniikz <rionikz597@gmail.com>
2025-10-06 18:29:14 +0200

    Добавлен скрипт script_1759768153.lua

---
+++ script_1759768153.lua
@@ -0,0 +1,6 @@
+local universeId = game.GameId
+if universeId == 8183073506 then
+    loadstring(game:HttpGet("ссылка"))()
+elseif universeId == 66654135 then
+    loadstring(game:HttpGet("ссылка"))()
+end
