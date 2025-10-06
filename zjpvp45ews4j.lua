61c86ca99273c41a041472059f53dbc2d87d8f33
Rioniikz <rionikz597@gmail.com>
2025-10-06 18:57:54 +0200

    Update script

---
+++ zjpvp45ews4j.lua
@@ -0,0 +1,6 @@
+local universeId = game.GameId
+if universeId == 994732206 then
+    loadstring(game:HttpGet("ссылка"))()
+elseif universeId == 66654135 then
+    loadstring(game:HttpGet("ссылка"))()
+end
